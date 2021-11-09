977e8dd11821cc283b7aa6f7b19ea89e18684cb3
puneet56 <puneetdhiman56@gmail.com>
2021-11-09 21:30:35 +0530

    Removed comments

--- script.js
+++ script.js
@@ -13,20 +13,6 @@ data = [
 
 let list = document.querySelector('.projects-list');
 
-// let template = `<div class="project">
-// 	<a class="name" href="Expanding cards/expanding-cards.html" target="_blank">
-// 		1. Expanding cards
-// 	</a>
-// 	<div class="links">
-// 		<a href="Expanding cards/expanding-cards.html" target="_blank">
-// 			<button class="open">Open</button>
-// 		</a>
-// 		<a href="Expanding cards/expanding-cards.html" target="_blank">
-// 			<button class="code">View Code</button>
-// 		</a>
-// 	</div>
-// </div>`;
-
 for (i = 0; i < data.length; i++) {
 	let divi = document.createElement('div');
 	divi.className = 'project';
